ab4cb0cbbca691c394bf67bfd70bc05e96fc6c42
李扬 <foryli@qq.com>
2023-05-19 02:30:19 +0800

    refactor the code

--- src/pages/_3_/_9_/ImageTemplateMatchPage.vue
+++ src/pages/_3_/_9_/ImageTemplateMatchPage.vue
@@ -5,16 +5,18 @@ const outputId = "outputId";
 
 function doCv() {
   const src = cvObj.imread(document.getElementById(inputId) as HTMLImageElement);
-  const templ = cvObj.imread(document.getElementById(inputTpltId) as HTMLImageElement);
+  const tplt = cvObj.imread(document.getElementById(inputTpltId) as HTMLImageElement);
   const dst = new cvObj.Mat();
   const mask = new cvObj.Mat();
-  cvObj.matchTemplate(src, templ, dst, cvObj.TM_CCOEFF, mask);
+  cvObj.matchTemplate(src, tplt, dst, cvObj.TM_CCOEFF, mask);
   const result = cvObj.minMaxLoc(dst, mask);
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   const maxPoint = result.maxLoc;
   const color = new cvObj.Scalar(255, 0, 0, 255);
-  const point = new cvObj.Point(maxPoint.x + templ.cols, maxPoint.y + templ.rows);
-  cvObj.rectangle(src, maxPoint, point, color, 2, cvObj.LINE_8, 0);
-  cvObj.imshow(document.getElementById(outputId) as HTMLCanvasElement, dst);
+  const point = new cvObj.Point(maxPoint.x + tplt.cols, maxPoint.y + tplt.rows);
+  cvObj.rectangle(src, maxPoint, point, color, 1, cvObj.LINE_8, 0);
+  cvObj.imshow(document.getElementById(outputId) as HTMLCanvasElement, src);
   src.delete();
   dst.delete();
   mask.delete();
