245409e44dd1be38696b412413472258f54b0419
李扬 <foryli@qq.com>
2023-05-18 23:53:15 +0800

    refactor the code

--- src/layouts/MainLayoutDrawerLeft.vue
+++ src/layouts/MainLayoutDrawerLeft.vue
@@ -1,17 +1,19 @@
 <script lang="ts" setup></script>
 
 <template>
-  <q-drawer behavior="desktop" elevated overlay>
-    <q-list>
-      <q-item v-for="i in $routeMainChildren" :key="i.path" v-ripple :to="i.path" clickable exact>
-        <q-item-section v-if="i.meta?.icon" avatar>
-          <q-icon :name="i.meta.icon" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>{{ i.meta.title }}</q-item-label>
-          <q-item-label caption>{{ i.meta.caption }}</q-item-label>
-        </q-item-section>
-      </q-item>
-    </q-list>
+  <q-drawer behavior="desktop" show-if-above elevated overlay>
+    <q-scroll-area class="fit">
+      <q-list>
+        <q-item v-for="i in $routeMainChildren" :key="i.path" v-ripple :to="i.path" clickable exact>
+          <q-item-section v-if="i.meta?.icon" avatar>
+            <q-icon :name="i.meta.icon" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ i.meta.title }}</q-item-label>
+            <q-item-label caption>{{ i.meta.caption }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-scroll-area>
   </q-drawer>
 </template>
