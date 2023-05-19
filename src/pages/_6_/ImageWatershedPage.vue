cb7ac8ff3b1631e3a542ba1a9472f11af34dc2fe
李扬 <foryli@qq.com>
2023-05-19 20:06:17 +0800

    refactor the code

--- src/pages/_6_/ImageWatershedPage.vue
+++ src/pages/_6_/ImageWatershedPage.vue
@@ -9,20 +9,56 @@ function doCv() {
   const gray = new cvObj.Mat();
   const opening = new cvObj.Mat();
   const coinsBg = new cvObj.Mat();
+  const coinsFg = new cvObj.Mat();
+  const distTrans = new cvObj.Mat();
+  const unknown = new cvObj.Mat();
+  const markers = new cvObj.Mat();
   cvObj.cvtColor(src, gray, cvObj.COLOR_RGBA2GRAY, 0);
   cvObj.threshold(gray, gray, 0, 255, cvObj.THRESH_BINARY_INV + cvObj.THRESH_OTSU);
-
+  // get background
   const M = new cvObj.Mat.ones(3, 3, cvObj.CV_8U);
   cvObj.erode(gray, gray, M);
   cvObj.dilate(gray, opening, M);
   cvObj.dilate(opening, coinsBg, M, new cvObj.Point(-1, -1), 3);
-
-  cvObj.imshow(document.getElementById(outputId) as HTMLCanvasElement, gray);
+  // distance transform
+  cvObj.distanceTransform(opening, distTrans, cvObj.DIST_L2, 5);
+  cvObj.normalize(distTrans, distTrans, 1, 0, cvObj.NORM_INF);
+  // get foreground
+  cvObj.threshold(distTrans, coinsFg, 0.7, 255, cvObj.THRESH_BINARY);
+  coinsFg.convertTo(coinsFg, cvObj.CV_8U, 1, 0);
+  cvObj.subtract(coinsBg, coinsFg, unknown);
+  // get connected components markers
+  cvObj.connectedComponents(coinsFg, markers);
+  for (let i = 0; i < markers.rows; i++) {
+    for (let j = 0; j < markers.cols; j++) {
+      markers.intPtr(i, j)[0] = markers.ucharPtr(i, j)[0] + 1;
+      if (unknown.ucharPtr(i, j)[0] == 255) {
+        markers.intPtr(i, j)[0] = 0;
+      }
+    }
+  }
+  cvObj.cvtColor(src, src, cvObj.COLOR_RGBA2RGB, 0);
+  cvObj.watershed(src, markers);
+  // draw barriers
+  for (let i = 0; i < markers.rows; i++) {
+    for (let j = 0; j < markers.cols; j++) {
+      if (markers.intPtr(i, j)[0] == -1) {
+        src.ucharPtr(i, j)[0] = 255; // R
+        src.ucharPtr(i, j)[1] = 0; // G
+        src.ucharPtr(i, j)[2] = 0; // B
+      }
+    }
+  }
+  cvObj.imshow(document.getElementById(outputId) as HTMLCanvasElement, src);
   src.delete();
   dst.delete();
   gray.delete();
   opening.delete();
   coinsBg.delete();
+  coinsFg.delete();
+  distTrans.delete();
+  unknown.delete();
+  markers.delete();
   M.delete();
 }
 </script>
