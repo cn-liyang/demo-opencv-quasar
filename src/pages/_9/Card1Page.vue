838816ed1ca83d8ebc4ce7df1a8c4207069a10a0
李扬 <foryli@qq.com>
2023-05-21 06:13:42 +0800

    refactor the code

--- src/pages/_9/Card1Page.vue
+++ src/pages/_9/Card1Page.vue
@@ -2,10 +2,13 @@
 const outputId = "outputId";
 
 async function asyncCvIoImageFile(file: File) {
-  const mat = cvObj.imread(await asyncPicaResizeImgFile2Canvas(file));
-
-  cvObj.imshow(document.getElementById(outputId) as HTMLCanvasElement, mat);
-  mat.delete();
+  const src = cvObj.imread(await asyncPicaResizeImgFile2Canvas(file));
+  const dst = new cvObj.Mat();
+  cvObj.cvtColor(src, src, cvObj.COLOR_RGBA2RGB, 0);
+  cvObj.bilateralFilter(src, dst, 9, 75, 75, cvObj.BORDER_DEFAULT);
+  cvObj.imshow(document.getElementById(outputId) as HTMLCanvasElement, dst);
+  src.delete();
+  dst.delete();
 }
 </script>
 
