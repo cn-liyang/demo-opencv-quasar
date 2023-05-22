9593477b5e186b93a2d9687c5868d0d1fe28126d
李扬 <foryli@qq.com>
2023-05-22 14:37:29 +0800

    refactor the code

--- src/pages/_9/Card2Page.vue
+++ src/pages/_9/Card2Page.vue
@@ -59,10 +59,9 @@ function doPoly1(edges: Mat, rows: number, cols: number) {
       cnt.delete();
       tmp.delete();
       break;
-    } else {
-      cnt.delete();
-      tmp.delete();
     }
+    cnt.delete();
+    tmp.delete();
   }
   const dark = new cvObj.Mat.zeros(rows, cols, cvObj.CV_8UC3);
   cvObj.fillPoly(dark, poly, new cvObj.Scalar(255, 0, 0));
@@ -73,7 +72,7 @@ function doPoly1(edges: Mat, rows: number, cols: number) {
 }
 
 function doLines(edge1: Mat) {
-  const linea = [];
+  const linea: Line[] = [];
   const lines = new cvObj.Mat();
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
@@ -81,10 +80,71 @@ function doLines(edge1: Mat) {
   for (let i = 0; i < lines.rows; ++i) {
     const rho = lines.data32F[i * 2];
     const theta = lines.data32F[i * 2 + 1];
-    console.log("Line", rho, theta);
     linea.push(new Line(rho, theta));
   }
   return linea;
+}
+
+function getIntersectionPoint(l1: Line, l2: Line) {
+  if (Math.abs(l1.theta - l2.theta) < 0.5) {
+    return;
+  }
+  const a1 =
+    Math.abs(l1.startPoint.x - l1.endPoint.x) < Number.EPSILON
+      ? 0
+      : (l1.startPoint.y - l1.endPoint.y) / (l1.startPoint.x - l1.endPoint.x);
+  const b1 = l1.startPoint.y - a1 * l1.startPoint.x;
+  const a2 =
+    Math.abs(l2.startPoint.x - l2.endPoint.x) < Number.EPSILON
+      ? 0
+      : (l2.startPoint.y - l2.endPoint.y) / (l2.startPoint.x - l2.endPoint.x);
+  const b2 = l2.startPoint.y - a2 * l2.startPoint.x;
+  if (Math.abs(a2 - a1) > Number.EPSILON) {
+    const x = (b1 - b2) / (a2 - a1);
+    const y = a1 * x + b1;
+    return new cvObj.Point(x, y);
+  }
+}
+
+function getDistance(p1: Point, p2: Point) {
+  const x = Math.abs(p1.x - p2.x);
+  const y = Math.abs(p1.y - p2.y);
+  return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+}
+
+function getCenter(points: Point[]) {
+  const x = points.reduce((sum, p) => sum + p.x, 0) / points.length;
+  const y = points.reduce((sum, p) => sum + p.y, 0) / points.length;
+  return new cvObj.Point(x, y);
+}
+
+function doPoint4(lines: Line[]) {
+  const points: Point[] = [];
+  for (let i = 0; i < lines.length; i++) {
+    for (let j = i + 1; j < lines.length; j++) {
+      const point = getIntersectionPoint(lines[i], lines[j]);
+      if (point) {
+        points.push(point);
+      }
+    }
+  }
+  const center = getCenter(points);
+  points.sort(
+    (a, b) =>
+      Math.atan((a.y - center.y) / (a.x - center.x || 0.01)) - Math.atan((b.y - center.y) / (b.x - center.x || 0.01))
+  );
+  const clusters: Point[][] = [[]];
+  for (let i = 1; i < points.length; i++) {
+    if (getDistance(points[i - 1], points[i]) < 30) {
+      clusters[clusters.length - 1].push(points[i]);
+    } else {
+      clusters.push([points[i]]);
+    }
+  }
+  return clusters
+    .sort((a, b) => b.length - a.length)
+    .slice(0, 4)
+    .map((i) => getCenter(i));
 }
 
 async function asyncCvIoImageFile(file: File) {
@@ -104,19 +164,11 @@ async function asyncCvIoImageFile(file: File) {
   });
   cvObj.imshow(document.getElementById(outputId) as HTMLCanvasElement, dark);
   dark.delete(); */
-  const points = [];
-  for (let i = 0; i < lines.length; i++) {
-    for (let j = i + 1; j < lines.length; j++) {
-      if (Math.abs(lines[i].theta - lines[j].theta) < 0.5) {
-        return;
-      }
-      /* let point = getIntersection(lines[i], lines[j]);
-      if (point) {
-        points.push(point);
-      } */
-    }
-  }
-
+  const point4 = doPoint4(lines);
+  console.log("point4", point4);
+  const dark = new cvObj.Mat.zeros(src.rows, src.cols, cvObj.CV_8UC3);
+  cvObj.rectangle(src, point4[0], point4[3], new cvObj.Scalar(255, 0, 0), 1, cvObj.LINE_8, 0);
+  cvObj.imshow(document.getElementById(outputId) as HTMLCanvasElement, src);
   src.delete();
   color.delete();
   edges.delete();
